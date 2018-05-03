5091ab163fb2ab0f1fdbff6ad92cfacfd4855c0f
陆玄青 <lu.xuanqing@outlook.com>
2018-05-03 16:33:53 +0800

    远程访问livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function(grunt) {
 		connect: {
 			server: {
 				options: {
-					hostname: '0.0.0.0',
+					hostname: '*',
 					port: port,
 					base: root,
 					livereload: true,
